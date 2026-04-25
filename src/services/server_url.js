a2bd1567da7e70ffec4ba504486fb012cb9746e8
catherine <catherinepgeorge02@gmail.com>
2026-04-25 17:33:05 +0530

    Update server URL

--- src/services/server_url.js
+++ src/services/server_url.js
@@ -1,2 +1,2 @@
 // export const server_url = "http://localhost:3000"
-export const server_url = import.meta.env.VITE_SERVER_URL || "https://bookserv-jia6.onrender.com"
+export const server_url = "https://bookserv-jia6.onrender.com"
